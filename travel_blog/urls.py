2b13787fc0574fec569aff328bb729db72cab7de
Lavang,Tejaswini <Tejaswini.Lavang@Cerner.com>
2017-03-30 15:06:13 -0500

    First commit

---
+++ travel_blog/urls.py
@@ -0,0 +1,11 @@
+from django.conf.urls import patterns, include, url
+from django.contrib import admin
+
+from . import views
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'myProject.views.home', name='home'),
+    # url(r'^blog/', include('blog.urls')),
+    url(r'^$', views.index, name='index'),
+)
